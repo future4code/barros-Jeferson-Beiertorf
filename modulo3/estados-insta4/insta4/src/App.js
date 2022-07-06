749c50b68174db19dbe8d9a1515c82aca4410f55
jeffbei <jefersonbeiertorf@gmail.com>
2022-07-05 22:38:32 -0300

    Atualização Insta4

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,32 +1,29 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Post from './components/Post/Post';
-import './style.css'
+import {MainContainer} from './style.css'
 
+// confesso que não sei se está certo,
+// não consegui pegar a aula de segunda-feira e estou sem cabeça pra pensar no código :( 
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'Paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
+    const listaPost = [
+      {nomeUsuario:'Paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost: 'https://picsum.photos/200/150'},
+      {nomeUsuario:'Goku', fotoUsuario:'https://picsum.photos/51/54', fotoPost: 'https://picsum.photos/200/151'},
+      {nomeUsuario:'Cebolinha', fotoUsuario:'https://picsum.photos/52/55', fotoPost: 'https://picsum.photos/201/156'},
+    ]
 
-          <Post
-            nomeUsuario={'Goku'}
-            fotoUsuario={'https://picsum.photos/51/54'}
-            fotoPost={'https://picsum.photos/200/151'}
-          />
-
-          <Post
-            nomeUsuario={'Cebolinha'}
-            fotoUsuario={'https://picsum.photos/52/55'}
-            fotoPost={'https://picsum.photos/201/156'}
-          />
-  </div>
-)
-
-}
+    const componentePost = listaPost.map ((valor, index) => {
+      return <li key={index}><Post nomeUsuario={valor.nomeUsuario} fotoUsuario=
+      {valor.fotoUsuario} fotoPost={valor.fotoPost}/></li>
+      })
+      
+      console.log (listaPost)
+      
+      return (
+ 
+          <ul> {componentePost}</ul>
+       
+      )}
 
 
 export default App;
