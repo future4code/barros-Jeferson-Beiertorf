c8f8f862ce37217f0e0dbd9f7fcf54ebf1bb5ed7
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-10-04 22:11:51 -0300

    Início exercícios revisão JS

---
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -0,0 +1,21 @@
+
+
+
+const user = {
+    name:"Tio Xico",
+    nickName: "xikinho",
+    password: 123456,
+    login: () => {
+        console.log("Login sucessful")
+    } 
+}
+user.login()
+
+
+const sum = (first, second) => {
+    return first + second
+}
+
+console.log(sum(3,6));
+
+// console.log("exercicio 4");
