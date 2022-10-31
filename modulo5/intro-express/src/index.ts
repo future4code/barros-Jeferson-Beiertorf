6f5275961557f836eb34b467bd8959f131c98d98
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-10-30 23:27:58 -0300

    exercícios introdução express

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,17 +1,46 @@
-import express from "express"
-
+import express,{Request, Response} from 'express'
 import cors from 'cors'
+import { postsList, usersList } from './data'
 
 const app = express()
 
 app.use(express.json())
-
 app.use(cors())
+
+
+app.get("/receba", (req:Request, res:Response) => {          
+    res.status(200).send("Hello from Express")
+})
+
+app.get('/users',(req:Request, res:Response)=>{
+    res.status(200).send(usersList)
+})
+app.get('/posts',(req:Request, res:Response)=>{
+    res.status(200).send(postsList)
+})
+app.get('/posts/:userId',(req:Request, res:Response)=>{
+    const postId =Number(req.query.idpost) 
+    const userId = Number(req.headers.iduser)
+    if(!postId && !userId){
+        res.status(400).send('Erro, obrigatório colocar ID')
+    }
+    const postFiltered = postsList.filter((post)=>{
+        return post.id === postId
+    })
+    const userFiltered = postsList.filter((user)=>{
+        return user.id === userId
+    })
+
+    const postUserList = console.log(postFiltered, userFiltered);
+    
+    res.status(200).send(postUserList)
+})
+
+
+
+
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
 });
-
-app.get("/hello", (req, res) =>{          
-    res.send("Hello from Express")
-})
---
+++ modulo5/intro-express/src/types.ts
@@ -0,0 +1,14 @@
+export type User = {
+    id: number,
+    name: string,
+    phone: string,
+    email: string,
+    website: string
+}
+
+export type Post = {
+    id: number ;
+    title: string;
+    body: string;
+    userId: number;
+}
