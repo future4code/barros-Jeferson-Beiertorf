9a13e06d3460806e89367dde9c467171b67aa22c
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2023-01-24 17:40:34 -0300

    initial

--- modulo5/arquitetura-software-3/labeflix(1)/labeflix/src/business/UserBusiness.ts
+++ modulo5/arquitetura-software-3/labeflix(1)/labeflix/src/business/UserBusiness.ts
@@ -19,3 +19,8 @@ export class UserBusiness {
   }
 
 }
+// - Exercício 1
+    
+//     Comecemos do começo, isto é, do momento em que recebemos uma requisição, um `Request` dos nossos endpoints;  A camada de controle é quem recebe estes dados. Para que tenhamos sempre a estrutura das informações que chegam, crie um modelo para isso. Neste caso, será um `DTO`. Um `DTO`, ou *Data Transfer Object* determina a estrutura dos dados. DTOs podem ser de entrada ou saída. Como estamos recebendo os dados, será um DTO de entrada. 
+    
+//     **Dessa forma, crie DTOs para serem utilizados nos endpoints criar usuário e criar filme.**
